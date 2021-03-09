3f3b5794aff25b64c2326f8e1372739d18614c5a
pareZhang <pareZhang@126.com>
2021-03-09 23:12:29 +0800

    服务提供方--服务注册发现

--- nacos-restful-provider/src/main/java/com/zjm/SpringRestProviderBootstrap.java
+++ nacos-restful-provider/src/main/java/com/zjm/SpringRestProviderBootstrap.java
@@ -2,12 +2,14 @@ package com.zjm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Author pareZhang
  * @Date 2021/3/7 22:06
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringRestProviderBootstrap {
     public static void main(String[] args) {
         SpringApplication.run(SpringRestProviderBootstrap.class);
