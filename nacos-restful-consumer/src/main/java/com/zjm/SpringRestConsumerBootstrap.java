b440b56a361f389a5f32fde001c9c139f94e997f
pareZhang <pareZhang@126.com>
2021-03-10 20:56:25 +0800

    服务注册发现--服务提供方（配置文件，启动类，controller）

--- nacos-restful-consumer/src/main/java/com/zjm/SpringRestConsumerBootstrap.java
+++ nacos-restful-consumer/src/main/java/com/zjm/SpringRestConsumerBootstrap.java
@@ -2,6 +2,8 @@ package com.zjm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -10,10 +12,15 @@ import org.springframework.web.client.RestTemplate;
  * @Date 2021/3/7 22:25
  **/
 @SpringBootApplication
+@EnableDiscoveryClient
 public class SpringRestConsumerBootstrap {
     public static void main(String[] args) {
         SpringApplication.run(SpringRestConsumerBootstrap.class);
     }
+    /**
+     *  @LoadBalanced 注解，开启 @LoadBalanced 与 Ribbon 的集成
+     */
+    @LoadBalanced
     @Bean
     public RestTemplate restTemplate(){
         return new RestTemplate();
--- nacos-restful-consumer/src/main/java/com/zjm/controller/RestConsumerController.java
+++ nacos-restful-consumer/src/main/java/com/zjm/controller/RestConsumerController.java
@@ -2,9 +2,15 @@ package com.zjm.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.net.URI;
 
 /**
  * @Author pareZhang
@@ -14,15 +20,24 @@ import org.springframework.web.client.RestTemplate;
 public class RestConsumerController {
     @Autowired
     private RestTemplate restTemplate;
-    //要进行远程调用，需要知道提供方的ip和端口
-    @Value("${provider.address}")
-    private String provider;
-
+    @Autowired
+    private LoadBalancerClient loadBalancerClient;
+    //指定服务名
+    String serviceId="nacos-restful-provider";
     @GetMapping(value = "/service")
     public String service(){
-        //远程调用
-        //RestTemplate restTemplate=new RestTemplate();
-        String providerResult =restTemplate.getForObject("http://" +provider+"/service",String.class);
-        return "consumer invoke |"+providerResult;
+        String result = restTemplate.getForObject("http://" + serviceId + "/service", String.class);
+        return "consumer | invoke " + result;
     }
+   /**
+    * 用此写法，启动类需要注释掉@LoadBalanced注解
+    @RequestMapping(value = "/service1",method = RequestMethod.GET)
+    public String service1(){
+        //发现一个地址
+        ServiceInstance serviceInstance=loadBalancerClient.choose(serviceId);
+        URI uri=serviceInstance.getUri();
+        String result1=restTemplate.getForObject(uri+"/service",String.class);
+        return "consumer invoke | "+result1;
+    }**/
+
 }
